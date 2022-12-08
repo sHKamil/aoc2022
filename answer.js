ca63b88ec0a5bf16552c5fd55fa7837ea7963a35
unknown <shootekb@gmail.com>
2022-12-08 16:08:26 +0100

    day - 2 (part 2)

--- answer.js
+++ answer.js
@@ -7,9 +7,8 @@ const m_scissors = "Z";
 const h_rock = "A";
 const h_paper = "B";
 const h_scissors = "C";
-var points = 0;
 
-const raw_strategy_guide = fs.readFileSync(file,'utf-8').replace(/\r/g, "").split("\n") // Get data from the file, replace hidden characters and split string every new line
+const raw_strategy_guide = fs.readFileSync(file,'utf-8').replace(/\r/g, "").split("\n"); // Get data from the file, replace hidden characters and split string every new line
 
 function cleanUp(raw_strategy){
     const strategy_guide = [];
@@ -20,6 +19,7 @@ function cleanUp(raw_strategy){
 }
 
 function pointsCounting(strategy){
+    let points = 0;
     strategy.forEach(element => { // Counting points for each round
         if(element[1]==m_rock)points=points+1;
         if(element[1]==m_paper)points=points+2;
@@ -33,6 +33,37 @@ function pointsCounting(strategy){
     return points;
 }
 
+function decryption(strategy){
+    const decrypt = [];
+    strategy.forEach((element, index) => { // Replace win/lose/draw (XYZ) to paper/rock/scissors marks (ABC)
+        if(element[1]=="Y") decrypt[index] = element.replace("Y",element[0]);
+        if(element[1]=="X"){
+            if(element[0]==h_rock) decrypt[index] = element.replace("X",h_scissors);
+            if(element[0]==h_paper) decrypt[index] = element.replace("X",h_rock);
+            if(element[0]==h_scissors) decrypt[index] = element.replace("X",h_paper);
+        }
+        if(element[1]=="Z"){
+            if(element[0]==h_rock) decrypt[index] = element.replace("Z",h_paper);
+            if(element[0]==h_paper) decrypt[index] = element.replace("Z",h_scissors);
+            if(element[0]==h_scissors) decrypt[index] = element.replace("Z",h_rock);
+        }  
+    });
+    return decrypt;
+}
 
+function decryptedStrategyCounting(strategy, decrypted){
+    let points = 0;
+    strategy.forEach(element => { // Counting points for wins and draws
+        if(element[1]=="Y") points = points + 3;
+        if(element[1]=="Z") points = points + 6;
+    });
+    decrypted.forEach(element => { // Counting points for using paper, rock and scissors
+        if(element[1]==h_rock)points=points+1;
+        if(element[1]==h_paper)points=points+2;
+        if(element[1]==h_scissors)points=points+3;
+    });
+    return points;
+}
 
 console.log("Part 1: "+pointsCounting(cleanUp(raw_strategy_guide)));
+console.log("Part 2: "+decryptedStrategyCounting(cleanUp(raw_strategy_guide),decryption(cleanUp(raw_strategy_guide))));
