00928ebab85eba3f8dddeaa08a0f3e93c74d261d
unknown <shootekb@gmail.com>
2022-12-16 13:21:31 +0100

    day - 3 (part 1 - fix)

--- day3/answer.js
+++ day3/answer.js
@@ -2,60 +2,16 @@ const fs = require('fs');
 const file = 'input.txt';
 const rucksacks = fs.readFileSync(file,'utf-8').replace(/\r/g, "").split("\n"); // Get data from the file, replace hidden characters and split string every new line
 
-const dictionary = {
-    "a": 1,
-    "b": 2,
-    "c": 3,
-    "d": 4,
-    "e": 5,
-    "f": 6,
-    "g": 7,
-    "h": 8,
-    "i": 9,
-    "j": 10,
-    "k": 11,
-    "l": 12,
-    "m": 13,
-    "n": 14,
-    "o": 15,
-    "p": 16,
-    "q": 17,
-    "r": 18,
-    "s": 19,
-    "t": 20,
-    "u": 21,
-    "v": 22,
-    "w": 23,
-    "x": 24,
-    "y": 25,
-    "z": 26,
-    "A": 27,
-    "B": 28,
-    "C": 29,
-    "D": 30,
-    "E": 31,
-    "F": 32,
-    "G": 33,
-    "H": 34,
-    "I": 35,
-    "J": 36,
-    "K": 37,
-    "L": 38,
-    "M": 39,
-    "N": 40,
-    "O": 41,
-    "P": 42,
-    "Q": 43,
-    "R": 44,
-    "S": 45,
-    "T": 46,
-    "U": 47,
-    "V": 48,
-    "W": 49,
-    "X": 50,
-    "Y": 51,
-    "Z": 52
-    };
+function makeAlphDictionary() {
+    let dict = {};
+    for (let i = 0; i < 26; i++) {
+        dict [String.fromCharCode(i+65).toLowerCase()]=i+1;
+    }
+    for (let i = 0; i < 26; i++) {
+        dict [String.fromCharCode(i+65)]=i+27;
+    }
+    return dict;
+}
 
 function typeCheck(array){
     const type = [];
@@ -84,6 +40,7 @@ function typeCheck(array){
 
 function pointsCounting(array){
     let points = 0;
+    const dictionary = makeAlphDictionary();
     array.forEach(element => {
         points = points + dictionary[element];
     });
