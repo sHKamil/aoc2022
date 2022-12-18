df4ce838bfe22da8926359f5fa7db677144c0426
unknown <shootekb@gmail.com>
2022-12-18 14:47:28 +0100

    day - 3 (part 2)

--- day3/answer.js
+++ day3/answer.js
@@ -1,3 +1,4 @@
+const { group } = require('console');
 const fs = require('fs');
 const file = 'input.txt';
 const rucksacks = fs.readFileSync(file,'utf-8').replace(/\r/g, "").split("\n"); // Get data from the file, replace hidden characters and split string every new line
@@ -14,19 +15,18 @@ function makeAlphDictionary() {
 }
 
 function typeCheck(array){
-    const type = [];
     let first = [];
     let second = [];
-    array.forEach((element, index) => {
+    array.forEach((element, index) => { // Divide in half each element
         first[index] = element.slice(0, element.length/2);
         second[index] = element.slice(element.length/2);
     });
     let temp = "";
     let types = 0;
     let keys = []; 
-    for (let f_i = 0; f_i < first.length; f_i++) {
+    for (let f_i = 0; f_i < first.length; f_i++) { // Compare divided parts to find repeating character
         temp =second[f_i];
-        for (let fs_i = 0; fs_i < first[f_i].length; fs_i++) {
+        for (let fs_i = 0; fs_i < first[f_i].length; fs_i++) { 
             types = typeof(temp[temp.indexOf(first[f_i][fs_i])])
             if(types!=typeof(undefined)) {
                 keys.push(temp[temp.indexOf(first[f_i][fs_i])]);
@@ -47,4 +47,37 @@ function pointsCounting(array){
     return points;
 }
 
-console.log(pointsCounting(typeCheck(rucksacks)));
+function groupByThree(array){
+    let groups = [];
+    for (let i = 0; i < array.length; i += 3){
+        groups.push(array.slice(i, i + 3));
+    }
+    return groups;
+}
+
+function findRepeatingChar(array) {
+    let temp = [];
+    let uniqueChars = [];
+    const rucksackKey = [];
+    for (let ai = 0; ai < array.length; ai++) {
+        for (let i = 0; i < array[ai][1].length; i++) {
+            if(array[ai][0].includes(array[ai][1][i])) {
+                temp.push(array[ai][1][i]);
+            }
+        }
+        uniqueChars = [...new Set(temp)]; // Remove duplicates
+        for (let i = 0; i < uniqueChars.length; i++) { // Check if character occurs in string 3 too
+            if(array[ai][2].includes(uniqueChars[i])) { // If yes put it in new array
+                rucksackKey.push(uniqueChars[i]) 
+            }
+        }
+        temp = [];
+        uniqueChars = [];
+
+    }
+    
+    return rucksackKey
+}
+
+console.log("Part 1: "+pointsCounting(typeCheck(rucksacks)));
+console.log("Part 2: "+pointsCounting(findRepeatingChar(groupByThree(rucksacks))));
